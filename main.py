d23c572eff531ae9d5b1dc8d147153d57f174ba6
sanya <79fz270704@gmail.com>
2020-07-03 11:17:20 +0300

    changes

--- main.py
+++ main.py
@@ -22,21 +22,12 @@ def list_sum(list1: list, list2: list) -> list:
     return list1 + list2
 
 
-l1 = [1, 3, 6, 8]
-l2 = [9, 0, 5, 3]
-
-result = list_sum(l1, l2)
-
-
 @args_doc_result
 def wo_dupe(input_list: list) -> list:
     """
     Удалите дубликаты из списка
     """
     return list(set(input_list))
-
-
-wo_dupe(result)
 
 
 @args_doc_result
@@ -47,10 +38,6 @@ def my_count(input_list: list) -> dict:
     return {el: input_list.count(el) for el in set(input_list)}
 
 
-d1 = [l for l in "".join(['A' * 5, 'B' * 3, 'C'])]
-my_count(d1)
-
-
 @args_doc_result
 def is_palindrome(word: str) -> bool:
     """
@@ -59,19 +46,12 @@ def is_palindrome(word: str) -> bool:
     return word == word[::-1]
 
 
-is_palindrome('abba')
-
-
 @args_doc_result
 def dif(list1: list, list2: list) -> list:
     """
     На вход получаете 2 списка, выведите новый список с элементами первого, которых нет во втором
     """
     return list(set(list1) - set(list2))
-
-
-nl1, nl2 = sample(ascii_letters, 5), sample(ascii_letters, 15)
-dif(nl1, nl2)
 
 
 @args_doc_result
@@ -85,17 +65,48 @@ def repetitive(string: str) -> bool:
     return False
 
 
-repetitive('samba')
+@args_doc_result
+def repetitive2(string: str) -> bool:
+    """
+    Проверьте, повторяются ли символы в строке, второй вариант
+    """
+    return len(string) is not len(set(string))
 
 
 @args_doc_result
 def frequent_and_rare(string: str) -> tuple:
     """
-    Найдите и выведите наиболее и наимее часто встречающеся слова в строке
+    Найдите и выведите наиболее и наименее часто встречающеся слова в строке
     """
     wc = my_count(string.split())
     min_count, max_count = min(wc.values()), max(wc.values())
     return [w for w, c in wc.items() if c is max_count], [w for w, c in wc.items() if c is min_count]
 
 
-frequent_and_rare('Найдите и выведите наиболее и наимее часто встречающеся слова в строке')
+if __name__ == '__main__':
+    #
+    l1 = [1, 3, 6, 8]
+    l2 = [9, 0, 5, 3]
+
+    result = list_sum(l1, l2)
+
+    #
+    wo_dupe(result)
+
+    #
+    d1 = [l for l in "".join(['A' * 5, 'B' * 3, 'C'])]
+    my_count(d1)
+
+    #
+    is_palindrome('abba')
+
+    #
+    nl1, nl2 = sample(ascii_letters, 5), sample(ascii_letters, 15)
+    dif(nl1, nl2)
+
+    #
+    repetitive('samba')
+    repetitive2('samba')
+
+    #
+    frequent_and_rare('Найдите и выведите наиболее и наимее часто встречающеся слова в строке')
